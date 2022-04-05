404e57cf42bac52a5422a370395a428fc1bc8be0
hamzakaraca <79093890+hamzakaraca@users.noreply.github.com>
2022-04-05 12:21:04 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Plaese Enter The First Number");
+            string number1 = Console.ReadLine();
+            Console.WriteLine("Plaese Enter Other Number");
+            string number2 = Console.ReadLine();
+
+            Add(Convert.ToInt32(number1),Convert.ToInt32(number2));
+        }
+        public static void  Add(int number1,int number2)
+        {
+            int result = number1 + number2;
+            Console.WriteLine("Result:" + result);
+        }
+    }
+}
